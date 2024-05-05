ae84c21211749e93fd97c2887f1fef3193531bce
Elizabeth <elhchiang@gmail.com>
2024-05-05 19:22:39 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ footer.appendChild(copyright);
 document.body.appendChild(footer);
 
 
-const skills = ["Doughnuts", "Fruits", "Vegetables", "Dim Sum Sets"];
+const skills = ["Tunisian Crochet", "Amigurumi", "Freeform Crochet", "Pattern Writing"];
 const skillsSection = document.getElementById("Skills");
 let skillsList = document.createElement("ul");
 skillsSection.appendChild(skillsList);
@@ -19,3 +19,61 @@ for (let skill of skills) {
   skillItem.innerText = skill;
   skillsList.appendChild(skillItem);
 }
+
+function onRemoveButton(event) {
+  console.log("Remove");
+  const entry = event.target.parentNode
+  entry.remove();
+
+const messageSection = document.getElementById("messages");
+const messageList = messageSection.getElementsByTagName("li");
+console.log(messageList);
+console.log(messageList.length);
+if (messageList.length === 0) {
+  messageSection.hidden = true;
+}
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+const data = new FormData (event.target);
+console.log(data);
+
+const userName = data.get("usersName");
+const email = data.get("email");
+const usersMessage = data.get("usersMessage");
+console.log(userName);
+console.log(email);
+console.log(usersMessage);
+
+const messageSection = document.getElementById("messages");
+const messageList = messageSection.getElementsByTagName("ul");
+console.log(messageList);
+
+messageSection.hidden = false;
+
+const newMessage = document.createElement("li");
+newString = `<a href="mailto:${email}">${userName}</a>\n
+<span>${usersMessage} </span>`;
+newMessage.innerHTML = newString;
+
+const removeButton = document.createElement("BUTTON");
+removeButton.innerText = "Remove";
+removeButton.setAttribute("type", "button");
+removeButton.setAttribute("id","removeButtonId");
+removeButton.addEventListener("click", onRemoveButton);
+console.log(removeButton);
+
+newMessage.appendChild(removeButton);
+
+messageList[0].appendChild(newMessage);
+
+event.target.reset();
+}
+
+const messageForms = document.getElementsByName("leave_message");
+console.log(messageForms);
+const messageForm = messageForms[0];
+console.log(messageForms);
+messageForm.addEventListener("submit", onFormSubmit);
